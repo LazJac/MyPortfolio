e3f7c6f2ba1165328e81623e75f1d5d060d77af0
AldrichDoggo <vwykjacoj@gmail.com>
2020-04-17 11:41:01 +0200

    Complete and Ready Ver.1 of website

--- Resume Website/Docs/Projects.js
+++ Resume Website/Docs/Projects.js
@@ -3,8 +3,8 @@
 
 const listProjects =                    //Hardcoding Project Units; Easier to add more later
 [
-  //[Link                                           ,  Start img           , End img]
-    ["https://github.com/LazJac"                    ,"Docs/Videos/start.gif"    ,"Docs/Videos/end.gif"]
+  //[Link, Cover img, Animated img]
+    ["https://github.com/LazJac/MyResume/tree/master/Resume%20Website","Docs/Videos/Resume_Cover.gif","Docs/Videos/Resume_Anim.gif"]
 ];
 
 const MakePatternForProjects = function(rankItem) //Project Units's HTML pattern for each item
@@ -14,7 +14,8 @@ const MakePatternForProjects = function(rankItem) //Project Units's HTML pattern
                       onmouseleave='ProjectVideoController(false,"Videos/start.gif",this)'
                       src="Docs/Videos/start.gif"
                       alt='projectImage' style='width: 52vw;height: 13vw;object-fit: scale-down;'>
-                </a></br>
+                </a></br></br>
+                <hr style='margin-left: auto;margin-right: auto;'>
 */
     let pattern = "</br></br><a href='" + rankItem[0] + "' target='_blank'>" +
                   "<img onmouseenter='ProjectVideoController(true, \"" + rankItem[2] + "\" ,this)'" +

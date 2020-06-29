e427fa86ddf9bf7f1037c10eb5acb7aa5c607db5
AldrichDoggo <vwykjacoj@gmail.com>
2020-06-29 18:14:40 +0200

    Removing less important stuff

--- BOIDS System with Movable boundaries/Enemies/Behaviors/SwarmBehavior.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SwarmBehavior : ScriptableObject
-{
-    public abstract Vector3 CalculateMove(Enemy enemy, List<Transform> context, SwarmNode swarm);
-}
